f732fa34aea40864e9eeef30a1fb2a16ec61e11c
Danila <danilabohanov@gmail.com>
2023-07-29 23:05:25 +0300

    remove list

--- hash_map.h
+++ hash_map.h
@@ -1,6 +1,5 @@
 #include <functional>
 #include <stdexcept>
-#include <list>
 
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType> > class HashMap {
 public:
@@ -25,49 +24,47 @@ public:
     }
 
     HashMap (const HashMap& other):hash_(other.hash_) {
-        if (storage_ != other.storage_) {
+        if (used_ != other.used_) {
             clear_memory();
             initial_memory(other.capacity_);
-            pairs_.clear();
-            used_positions_.clear();
 
             for (size_t i = 0; i < capacity_; i++) {
                 if (other.used_[i] == 1) {
-                    create_pair(i, other.storage_[i] -> first, other.storage_[i] -> second);
-                } else if (other.used_[i] == 2) {
-                    current_workload_++;
+                    check_overload();
+                    create_pair(i, other.pairs_[i].first, other.pairs_[i].second);
                 }
-                used_[i] = other.used_[i];
             }
         }
     }
 
     HashMap& operator=(const HashMap& other) {
-        if (storage_ != other.storage_) {
+        if (used_ != other.used_) {
             clear_memory();
             initial_memory(other.capacity_);
-            pairs_.clear();
-            used_positions_.clear();
 
             for (size_t i = 0; i < capacity_; i++) {
                 if (other.used_[i] == 1) {
-                    create_pair(i, other.storage_[i] -> first, other.storage_[i] -> second);
-                } else if (other.used_[i] == 2) {
-                    current_workload_++;
+                    check_overload();
+                    create_pair(i, other.pairs_[i].first, other.pairs_[i].second);
                 }
-                used_[i] = other.used_[i];
             }
         }
         return *this;
     }
 
     ~HashMap() {
-        delete []storage_;
         delete []used_;
-        delete []mark_indexes_;
+        delete []new_used_;
+        delete []pairs_;
+        delete []new_pairs_;
+        used_ = nullptr;
+        new_used_ = nullptr;
+        pairs_ = nullptr;
+        new_pairs_ = nullptr;
     }
 
     void insert(const std::pair<KeyType, ValueType>& item) {
+        check_overload();
         size_t index = find_position(item.first);
         if (used_[index] == 1) {
             return;
@@ -80,19 +77,19 @@ public:
         if (used_[index] != 1) {
             return;
         }
-        pairs_.erase(storage_[index]);
-        used_positions_.erase(mark_indexes_[index]);
-        used_[index] = 2;
-        size_--;
+        delete_pair(index);
+        check_insufficient_load();
     }
+
     ValueType& operator[](const KeyType& key) {
         size_t index = find_position(key);
         if (used_[index] != 1) {
-            if (create_pair(index, key)) {
-                return storage_[find_position(key)] -> second;
+            if (check_overload()) {
+                index = find_position(key);
             }
+            create_pair(index, key, ValueType{});
         }
-        return storage_[index] -> second;
+        return pairs_[index].second;
     };
 
     const ValueType& at(const KeyType& key) const {
@@ -100,7 +97,7 @@ public:
         if (used_[index] != 1) {
             throw std::out_of_range ("The key doesn't exist");
         }
-        return storage_[index] -> second;
+        return pairs_[index].second;
     };
 
     size_t size() const {
@@ -112,29 +109,120 @@ public:
     }
 
     void clear() {
-        for (auto j : used_positions_) {
-            used_[j] = 2;
-        }
-        size_ = 0;
-        used_positions_.clear();
-        pairs_.clear();
+        clear_memory();
+        initial_memory(INITIAL_SIZE);
     }
 
-    using iterator = typename std::list<std::pair<const KeyType, ValueType>>::iterator;
-    using const_iterator = typename std::list<std::pair<const KeyType, ValueType>>::const_iterator;
+    Hash hash_function() const {
+        return hash_;
+    }
+
+    class iterator {
+    public:
+        iterator(): ptr_pair_(nullptr), ptr_used_(nullptr), end_used_(nullptr) {}
+        iterator(std::pair<KeyType, ValueType> *ptr_pair, uint8_t *ptr_used, uint8_t *end_used) :
+        ptr_pair_((std::pair<const KeyType, ValueType> *)ptr_pair), ptr_used_(ptr_used), end_used_(end_used) {}
+
+        std::pair<const KeyType, ValueType>& operator*() {
+            return *ptr_pair_;
+        }
+
+        std::pair<const KeyType, ValueType>* operator->() {
+            return ptr_pair_;
+        }
+
+        iterator& operator++() {
+            ++ptr_pair_;
+            while (*(++ptr_used_) != 1 && ptr_used_ != end_used_) {
+                ++ptr_pair_;
+            }
+            return *this;
+        }
+        iterator operator++(int) {
+            iterator cur = *this;
+            ++ptr_pair_;
+            while (*(++ptr_used_) != 1 && ptr_used_ != end_used_) {
+                ++ptr_pair_;
+            }
+            return cur;
+        }
+
+        friend bool operator==(const iterator &a, const iterator &b) {
+            return a.ptr_used_ == b.ptr_used_ && a.ptr_pair_ == b.ptr_pair_;
+        }
+
+        friend bool operator!=(const iterator &a, const iterator &b) {
+            return a.ptr_used_ != b.ptr_used_ || a.ptr_pair_ != b.ptr_pair_;
+        }
+
+    private:
+        std::pair<const KeyType, ValueType> *ptr_pair_ = nullptr;
+        uint8_t *ptr_used_ = nullptr, *end_used_ = nullptr;
+    };
+
+    class const_iterator {
+    public:
+        const_iterator(): ptr_pair_(nullptr), ptr_used_(nullptr), end_used_(nullptr) {}
+        const_iterator(const std::pair<KeyType, ValueType> *ptr_pair, uint8_t *ptr_used, uint8_t *end_used) :
+        ptr_pair_((const std::pair<const KeyType, ValueType>*)ptr_pair), ptr_used_(ptr_used), end_used_(end_used) {}
+
+        const std::pair<const KeyType, ValueType>& operator*() {
+            return *ptr_pair_;
+        }
+
+        const std::pair<const KeyType, ValueType>* operator->() {
+            return ptr_pair_;
+        }
+
+        const_iterator& operator++() {
+            ++ptr_pair_;
+            while (*(++ptr_used_) != 1 && ptr_used_ != end_used_) {
+                ++ptr_pair_;
+            }
+            return *this;
+        }
+        const_iterator operator++(int) {
+            const_iterator cur = *this;
+            ++ptr_pair_;
+            while (*(++ptr_used_) != 1 && ptr_used_ != end_used_) {
+                ++ptr_pair_;
+            }
+            return cur;
+        }
+
+        friend bool operator==(const const_iterator &a, const const_iterator &b) {
+            return a.ptr_used_ == b.ptr_used_ && a.ptr_pair_ == b.ptr_pair_;
+        }
+
+        friend bool operator!=(const const_iterator &a, const const_iterator &b) {
+            return a.ptr_used_ != b.ptr_used_ || a.ptr_pair_ != b.ptr_pair_;
+        }
+
+    private:
+        const std::pair<const KeyType, ValueType> *ptr_pair_ = nullptr;
+        uint8_t *ptr_used_ = nullptr, *end_used_ = nullptr;
+    };
 
     iterator begin() {
-        return iterator(pairs_.begin());
+        uint8_t* begin = used_;
+        while (*(begin) != 1 && begin != used_ + capacity_) {
+            ++begin;
+        }
+        return iterator(pairs_ + (begin - used_), begin, used_ + capacity_);
     }
     iterator end() {
-        return iterator(pairs_.end());
+        return iterator(pairs_ + capacity_, used_ + capacity_, used_ + capacity_);
     }
 
     const_iterator begin() const {
-        return const_iterator(pairs_.begin());
+        uint8_t* begin = used_;
+        while (*(begin) != 1 && begin != used_ + capacity_) {
+            ++begin;
+        }
+        return const_iterator(pairs_ + (begin - used_), begin, used_ + capacity_);
     }
     const_iterator end() const {
-        return const_iterator(pairs_.end());
+        return const_iterator(pairs_ + capacity_, used_ + capacity_, used_ + capacity_);
     }
 
     const_iterator find(const KeyType& key) const {
@@ -142,51 +230,44 @@ public:
         if (used_[index] != 1) {
             return end();
         }
-        return const_iterator(storage_[index]);
+        return const_iterator(pairs_ + index, used_ + index, used_ + capacity_);
     }
+
     iterator find(const KeyType& key) {
         size_t index = find_position(key);
         if (used_[index] != 1) {
             return end();
         }
-        return iterator(storage_[index]);
+        return iterator(pairs_ + index, used_ + index, used_ + capacity_);
     }
 
-    Hash hash_function() const {
-        return hash_;
-    }
 private:
-    constexpr static const size_t INITIAL_SIZE = 16;
+    constexpr static const size_t INITIAL_SIZE = 2;
     constexpr static const size_t SHIFT_HASH_FACTORS[] = {239, 179, 191};
+    constexpr static const size_t BOTTOM_LOAD_FACTOR = 25;
     constexpr static const size_t TOP_LOAD_FACTOR = 50;
     constexpr static const size_t MAX_PERCENT = 100;
     Hash hash_;
-    std::list<std::pair<const KeyType, ValueType>> pairs_;
-    std::list<size_t> used_positions_;
-    typename std::list<std::pair<const KeyType, ValueType>>::iterator* storage_ = nullptr;
-    typename std::list<size_t>::iterator* mark_indexes_ = nullptr;
+    std::pair<KeyType, ValueType>* pairs_, *new_pairs_;
     size_t size_;
-    size_t current_workload_;
     size_t capacity_;
-    char* used_ = nullptr;
+    uint8_t * used_ = nullptr;
+    uint8_t * new_used_ = nullptr;
     void initial_memory(size_t new_capacity) {
-        storage_ = new typename  std::list<std::pair<const KeyType, ValueType>>
-        ::iterator[new_capacity];
+        pairs_ = new std::pair<KeyType, ValueType> [new_capacity];
         size_ = 0;
-        current_workload_ = 0;
         capacity_ = new_capacity;
-        used_ = new char[new_capacity];
-        mark_indexes_ = new typename  std::list<size_t>
-        ::iterator[new_capacity];
+        used_ = new uint8_t [new_capacity];
         for (size_t i = 0; i < capacity_; i++) {
             used_[i] = 0;
         }
     }
 
     void clear_memory() {
-        delete []storage_;
         delete []used_;
-        delete []mark_indexes_;
+        delete []pairs_;
+        used_ = nullptr;
+        pairs_ = nullptr;
     }
 
     size_t compute_shift_hash(size_t primary_hash) const {
@@ -200,63 +281,70 @@ private:
     size_t find_position(const KeyType& key) const {
         size_t hash = hash_(key), shift_hash = compute_shift_hash(hash);
         size_t index = hash % capacity_;
-        while (used_[index] == 2 || (used_[index] == 1 && !(storage_[index] -> first == key))) {
+        size_t first_zero = capacity_;
+        while (used_[index] == 2 || (used_[index] == 1 && (pairs_[index].first != key))) {
+            if (first_zero == capacity_ && used_[index] == 2) {
+                first_zero = index;
+            }
             index = (index + shift_hash) % capacity_;
+        }
+        if (used_[index] == 1 || pairs_[index].first == key) {
+            return index;
+        }
+        if (first_zero != capacity_) {
+            return first_zero;
         }
         return index;
     }
 
-    bool create_pair(size_t index, const KeyType& key, const ValueType& value = ValueType()) {
-        pairs_.emplace_back(key, value);
-        storage_[index] = --pairs_.end();
-        used_positions_.push_back(index);
-        mark_indexes_[index] = --used_positions_.end();
+    void create_pair(size_t index, const KeyType& key, const ValueType& value = ValueType()) {
+        pairs_[index].first = key;
+        pairs_[index].second = value;
         size_++;
-        current_workload_++;
         used_[index] = 1;
-        if (check_overload()) {
+    }
+
+    void delete_pair(size_t index) {
+        pairs_[index] = {KeyType{}, ValueType{}};
+        size_--;
+        used_[index] = 2;
+    }
+
+    bool check_overload() {
+        if (MAX_PERCENT * (size_ + 1) > TOP_LOAD_FACTOR * capacity_) {
             rebuild(capacity_ * 2);
             return true;
         }
         return false;
     }
 
-    bool check_overload() const {
-        return MAX_PERCENT * current_workload_ > TOP_LOAD_FACTOR * capacity_;
+    void check_insufficient_load() {
+        if (size_ >= 2 * INITIAL_SIZE && MAX_PERCENT * size_ < BOTTOM_LOAD_FACTOR * capacity_) {
+            rebuild(capacity_ / 2);
+        }
     }
 
     void rebuild(size_t new_capacity) {
-        typename std::list<std::pair<const KeyType, ValueType>>::iterator* new_storage =
-                new typename std::list<std::pair<const KeyType, ValueType>>
-                ::iterator[new_capacity];
-        typename std::list<size_t>::iterator* new_marked_indexes = new typename  std::list<size_t>
-        ::iterator[new_capacity];
-
         size_t new_size = 0;
-        size_t new_current_workload = 0;
-        char* new_used = new char [new_capacity];
+        new_used_ = new uint8_t [new_capacity];
         for (size_t i = 0; i < new_capacity; i++) {
-            new_used[i] = 0;
+            new_used_[i] = 0;
         }
-        std::list<std::pair<const KeyType, ValueType>> new_pairs;
+        new_pairs_ = new std::pair<KeyType, ValueType> [new_capacity];
 
-        std::list<size_t> new_used_positions;
         std::swap(new_capacity, capacity_);
-        std::swap(new_storage, storage_);
         std::swap(new_size, size_);
-        std::swap(new_current_workload, current_workload_);
-        std::swap(new_used, used_);
-        std::swap(new_used_positions, used_positions_);
-        std::swap(new_marked_indexes, mark_indexes_);
-        std::swap(new_pairs, pairs_);
+        std::swap(new_used_, used_);
+        std::swap(new_pairs_, pairs_);
         for (size_t i = 0; i < new_capacity; i++) {
-            if (new_used[i] == 1) {
-                size_t index = find_position(new_storage[i] -> first);
-                create_pair(index, new_storage[i] -> first, new_storage[i] -> second);
+            if (new_used_[i] == 1) {
+                size_t index = find_position(new_pairs_[i].first);
+                create_pair(index, new_pairs_[i].first, new_pairs_[i].second);
             }
         }
-        delete []new_storage;
-        delete []new_used;
-        delete []new_marked_indexes;
+        delete []new_used_;
+        new_used_ = nullptr;
+        delete []new_pairs_;
+        new_pairs_ = nullptr;
     }
 };
